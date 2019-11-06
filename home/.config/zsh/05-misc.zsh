1b64ba84f18bfcfc75f64a81a1ce0e46caed4a74
Marko Korhonen <marko.korhonen@reekynet.com>
2019-11-06 08:36:20 +0200

    Added zsh history sharing
    Signed-off-by: Marko Korhonen <marko.korhonen@reekynet.com>

--- home/.config/zsh/05-misc.zsh
+++ home/.config/zsh/05-misc.zsh
@@ -2,3 +2,9 @@
 if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
 	tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
 fi
+
+# share history between running zsh instances
+setopt share_history
+
+# ignore commands with leading space from history
+setopt histignorespace

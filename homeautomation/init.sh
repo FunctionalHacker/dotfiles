a4483bc8ba6ffb50b96519954a9edb4d1c6aebdd
Marko Korhonen <marko.korhonen@reekynet.com>
2019-11-10 21:29:03 +0200

    Added mariadb
    Signed-off-by: Marko Korhonen <marko.korhonen@reekynet.com>

--- homeautomation/init.sh
+++ homeautomation/init.sh
@@ -4,9 +4,9 @@ sudo mkdir -p /homeautomation-config/node-red
 sudo chmod -R a+rw /homeautomation-config/node-red
 
 cat <<ENV > .env
-MYSQL_ROOT_PASSWORD="$(pass reekynet/docker-home-automation | rg 'MYSQL_ROOT_PASSWORD' | cut -d' ' -f2-)"
-HA_MYSQL_PASSWORD="$(pass reekynet/docker-home-automation | rg 'HA_MYSQL_PASSWORD' | cut -d' ' -f2-)"
-DECONZ_VNC_PASSWORD="$(pass reekynet/docker-home-automation | rg 'DECONZ_VNC_PASSWORD' | cut -d' ' -f2-)"
+MYSQL_ROOT_PASSWORD=$(pass reekynet/docker-home-automation | rg 'MYSQL_ROOT_PASSWORD' | cut -d' ' -f2-)
+HA_MYSQL_PASSWORD="$(pass reekynet/docker-home-automation | rg 'HA_MYSQL_PASSWORD' | cut -d' ' -f2-)
+DECONZ_VNC_PASSWORD=$(pass reekynet/docker-home-automation | rg 'DECONZ_VNC_PASSWORD' | cut -d' ' -f2-)
 ENV
 
 docker-compose up -d

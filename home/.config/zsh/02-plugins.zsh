ac77396514a8a067c8fee7ffa712d6aae3444a75
Marko Korhonen <marko@korhonen.cc>
2020-01-19 01:36:21 +0200

    Zplug -> zinit name change
    Signed-off-by: Marko Korhonen <marko.korhonen@reekynet.com>

--- home/.config/zsh/02-plugins.zsh
+++ home/.config/zsh/02-plugins.zsh
@@ -1,46 +1,46 @@
-# install zplugin if it's not already installed
-if [ ! -f ~/.zplugin/bin/zplugin.zsh ]; then
-	mkdir ~/.zplugin
-	git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
+# install zinit if it's not already installed
+if [ ! -f ~/.zinit/bin/zinit.zsh ]; then
+	mkdir ~/.zinit
+	git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
 fi
 
-source ~/.zplugin/bin/zplugin.zsh
+source ~/.zinit/bin/zinit.zsh
 
 # pure prompt
-zplugin ice load multisrc'{async,pure}.zsh'
-zplugin light sindresorhus/pure
+zinit ice load multisrc'{async,pure}.zsh'
+zinit light sindresorhus/pure
 
 # fzf as tab completion
-zplugin ice lucid
-zplugin light Aloxaf/fzf-tab
+zinit ice lucid
+zinit light Aloxaf/fzf-tab
 
 # substring search
-zplugin ice lucid
-zplugin light zsh-users/zsh-history-substring-search
+zinit ice lucid
+zinit light zsh-users/zsh-history-substring-search
 
 # fzf git awesomeness
-zplugin ice wait'1' lucid atload'source $HOME/.config/zsh/03-aliases.zsh'
-zplugin light wfxr/forgit
+zinit ice wait'1' lucid atload'source $HOME/.config/zsh/03-aliases.zsh'
+zinit light wfxr/forgit
 
 # lots of completions
-zplugin ice wait'1' lucid as'completion'
-zplugin light zsh-users/zsh-completions
+zinit ice wait'1' lucid as'completion'
+zinit light zsh-users/zsh-completions
 
 # swaymsg completions
-zplugin ice wait'1' lucid as'completion'
-zplugin snippet https://gist.githubusercontent.com/RPigott/a3efdc08fa4d5fd7df38d091ab5e8532/raw/867ac1884d97d1bd1e5d4037d4eefdf4a004e72b/_swaymsg
+zinit ice wait'1' lucid as'completion'
+zinit snippet https://gist.githubusercontent.com/RPigott/a3efdc08fa4d5fd7df38d091ab5e8532/raw/867ac1884d97d1bd1e5d4037d4eefdf4a004e72b/_swaymsg
 
 # dotdrop completion
-zplugin ice wait'1' lucid as'completion'
-zplugin snippet '$DOTREPO/dotdrop/completion/_dotdrop-completion.zsh'
+zinit ice wait'1' lucid as'completion'
+zinit snippet '$DOTREPO/dotdrop/completion/_dotdrop-completion.zsh'
 
 # docker-compose completion
-zplugin ice wait'1' lucid as'completion'
-zplugin snippet https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose
+zinit ice wait'1' lucid as'completion'
+zinit snippet https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose
 
 # syntax highlighting
-zplugin ice lucid atinit'zpcompinit'
-zplugin light zdharma/fast-syntax-highlighting
+zinit ice lucid atinit'zpcompinit'
+zinit light zdharma/fast-syntax-highlighting
 
 # use fzf with zsh
 source /usr/share/fzf/key-bindings.zsh

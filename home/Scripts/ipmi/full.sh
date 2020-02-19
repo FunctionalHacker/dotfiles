90fc7f56ec36dc8b161f8cc5ff243f137002395d
Marko Korhonen <marko@korhonen.cc>
2020-02-19 20:33:15 +0200

    Add full speed fan script

---
+++ home/Scripts/ipmi/full.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+IPMIHOST=10.0.0.82
+IPMIUSER=ReekyMarko
+IPMIPW={{@@ env['PASS_IPMI'] @@}}
+IPMIEK=0000000000000000000000000000000000000000
+
+printf "speeeed!"
+ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
+ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x64

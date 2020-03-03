33334017164fe1a9e6f0891549fe0ed7f8307907
Marko Korhonen <marko.korhonen@reekynet.com>
2020-03-03 16:54:47 +0200

    Trying out a lower static speed

--- home/Scripts/ipmi/static.sh
+++ home/Scripts/ipmi/static.sh
@@ -7,4 +7,4 @@ IPMIEK=0000000000000000000000000000000000000000
 
 printf "Activating manual fan speeds! (2160 RPM)"
 ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
-ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x11
+ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x10

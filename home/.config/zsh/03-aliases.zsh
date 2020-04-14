338e28e8c6db591807b3f243b5003cad06dee46f
Marko Korhonen <marko@korhonen.cc>
2020-04-14 22:41:49 +0300

    Fix docker update function name

--- home/.config/zsh/03-aliases.zsh
+++ home/.config/zsh/03-aliases.zsh
@@ -120,7 +120,7 @@ update() {
 		plugins
 		{%@@ if profile == "Moria" @@%}
 		base --devel
-		docker
+		docker-pull
 		{%@@ elif profile == "Mirkwood" @@%}
 		base --devel firefox-nightly
 		{%@@ else @@%}
@@ -142,7 +142,7 @@ update() {
 	}
 	{%@@ if profile == "Moria" @@%}
 
-	docker() {
+	docker-pull() {
 		for dir in $HOME/Git/dotfiles/docker/*; do
 			cd $dir
 			docker-compose pull
@@ -168,7 +168,7 @@ update() {
 			;;
 	{%@@ if profile == "Moria" @@%}
 		docker)
-			docker
+			docker-pull
 			;;
 	{%@@ endif @@%}
 		*)

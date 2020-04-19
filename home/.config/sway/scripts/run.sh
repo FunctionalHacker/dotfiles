8dece7e5139aea35ad94f7626a735b4a954e65f2
Marko Korhonen <marko.korhonen@reekynet.com>
2020-04-19 11:54:48 +0300

    Shouldn't copy paste stuff blindly

--- home/.config/sway/scripts/run.sh
+++ home/.config/sway/scripts/run.sh
@@ -16,7 +16,7 @@ export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswi
 
 # Enable qt5 theming and wayland support
 export QT_QPA_PLATFORMTHEME=qt5ct
-export QT_QPA_PLATFORM=wayland-egl
+export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 
 # Fix redshift on wlroots
@@ -26,13 +26,13 @@ export WLR_DRM_NO_ATOMIC=1
 sway
 
 # Unset the variables after quitting Sway
-unset MOZ_ENABLE_WAYLAND=1
-unset MOZ_USE_XINPUT2=1
-unset MOZ_WEBRENDER=1
-unset _JAVA_AWT_WM_NONREPARENTING=1
-unset _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-unset QT_QPA_PLATFORMTHEME=qt5ct
-unset QT_QPA_PLATFORM=wayland-egl
-unset QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-unset WLR_DRM_NO_ATOMIC=1
-unset XDG_CURRENT_DESKTOP=sway
+unset MOZ_ENABLE_WAYLAND
+unset MOZ_USE_XINPUT2
+unset MOZ_WEBRENDER
+unset _JAVA_AWT_WM_NONREPARENTING
+unset _JAVA_OPTIONS
+unset QT_QPA_PLATFORMTHEME
+unset QT_QPA_PLATFORM
+unset QT_WAYLAND_DISABLE_WINDOWDECORATION
+unset WLR_DRM_NO_ATOMIC
+unset XDG_CURRENT_DESKTOP

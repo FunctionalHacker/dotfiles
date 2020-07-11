543bf3be0c492bf64feb81ee22ce01cbed3ae0d5
Marko Korhonen <marko@korhonen.cc>
2020-07-11 12:24:17 +0300

    Remove init script

--- docker/homeautomation/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat <<ENV > .env
-MYSQL_ROOT_PASSWORD=$(pass reekynet/docker-home-automation | rg 'MYSQL_ROOT_PASSWORD' | cut -d' ' -f2-)
-HA_MYSQL_PASSWORD="$(pass reekynet/docker-home-automation | rg 'HA_MYSQL_PASSWORD' | cut -d' ' -f2-)
-DECONZ_VNC_PASSWORD=$(pass reekynet/docker-home-automation | rg 'DECONZ_VNC_PASSWORD' | cut -d' ' -f2-)
-ENV
-
-docker-compose up -d

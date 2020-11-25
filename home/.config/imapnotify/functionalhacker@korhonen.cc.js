a38b58d91a9797dfa6a796f62cb2887d41157113
Marko Korhonen <marko.korhonen@reekynet.com>
2020-11-25 10:06:19 +0200

    Working on neomutt setup and some other small fixes

---
+++ home/.config/imapnotify/functionalhacker@korhonen.cc.js
@@ -0,0 +1,19 @@
+var child_process = require("child_process");
+
+function getStdout(cmd) {
+  var stdout = child_process.execSync(cmd);
+  return stdout.toString().trim();
+}
+
+exports.host = "imap.migadu.com";
+exports.port = 993;
+exports.tls = true;
+exports.tlsOptions = {
+  rejectUnauthorized: true,
+};
+exports.username = "functionalhacker@korhonen.cc";
+exports.password = "{{@@ env['PASS_EMAIL_HACKER'] @@}}";
+exports.onNewMail = "mbsync functionalhacker@korhonen.cc";
+exports.onNewMailPost =
+  "~/git/dotfiles/scripts/mail/notify-new-mail.sh functionalhacker@korhonen.cc";
+exports.boxes = ["INBOX"];

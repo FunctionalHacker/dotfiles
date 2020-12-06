e25601f71ed745e4f43570faecd4d3d87e9ad293
Marko Korhonen <marko@korhonen.cc>
2020-12-06 11:39:02 +0200

    Small fixes

---
+++ home/.config/neomutt/accounts/marko@korhonen.cc
@@ -0,0 +1,9 @@
+# vim: filetype=neomuttrc
+
+set folder = ~/.mail/marko@korhonen.cc
+set spoolfile = +INBOX
+set postponed = +Drafts
+set record = +Sent
+
+set realname='Marko Korhonen'
+set from=marko@korhonen.cc

dca64d7a4c87e79fab65efcdef3cd2cbba7c3558
Marko Korhonen <marko@korhonen.cc>
2020-12-17 00:03:06 +0200

    Fix gpg env setup

--- home/.config/zsh/01-env.zsh
+++ home/.config/zsh/01-env.zsh
@@ -36,8 +36,8 @@ export PAGER="nvimpager -p"
 
 # use gpg for ssh
 export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-gpg-connect-agent updatestartuptty /bye > /dev/null
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
 
 # tehfuk
 eval $(thefuck --alias)

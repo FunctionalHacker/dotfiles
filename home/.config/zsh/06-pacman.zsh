da9abb7282280752329080a75a2579ecc0d253b4
Marko Korhonen <marko@korhonen.cc>
2021-08-17 20:54:45 +0300

    Activate zsh rehash with pacman hook

--- home/.config/zsh/06-pacman.zsh
+++ home/.config/zsh/06-pacman.zsh
@@ -1,6 +1,17 @@
 # from https://wiki.archlinux.org/index.php/Zsh#On-demand_rehash
-catch_signal_usr1() {
-  trap catch_signal_usr1 USR1
-  rehash
+
+zshcache_time="$(date +%s%N)"
+
+autoload -Uz add-zsh-hook
+
+rehash_precmd() {
+  if [[ -a /var/cache/zsh/pacman ]]; then
+    local paccache_time="$(date -r /var/cache/zsh/pacman +%s%N)"
+    if (( zshcache_time < paccache_time )); then
+      rehash
+      zshcache_time="$paccache_time"
+    fi
+  fi
 }
-trap catch_signal_usr1 USR1
+
+add-zsh-hook -Uz precmd rehash_precmd

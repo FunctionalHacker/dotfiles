bf1c9234e2a0284489b7c6beffe6c34bcc3bef68
Marko Korhonen <marko@korhonen.cc>
2021-12-03 09:30:27 +0200

    Neovim: Enable indent-blankline

--- home/.config/nvim/lua/pluginconf/indent-blankline.lua
+++ home/.config/nvim/lua/pluginconf/indent-blankline.lua
@@ -1,15 +1,9 @@
-require('indent_blankline').setup {char = " ", filetype_exclude = {'help', 'terminal'}}
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
 
--- define the highlight groups with only background colors (or leave odd empty to just show the normal background)
-vim.cmd [[highlight IndentOdd guifg=NONE guibg=NONE gui=nocombine]]
-vim.cmd [[highlight IndentEven guifg=NONE guibg=#354254 gui=nocombine]]
--- and then use the highlight groups
-vim.g.indent_blankline_char_highlight_list = {"IndentOdd", "IndentEven"}
-vim.g.indent_blankline_space_char_highlight_list = {"IndentOdd", "IndentEven"}
-
--- don't show any characters
-vim.g.indent_blankline_char = " "
-vim.g.indent_blankline_space_char = " "
-
--- when using background, the trailing indent is not needed / looks wrong
-vim.g.indent_blankline_show_trailing_blankline_indent = false
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}

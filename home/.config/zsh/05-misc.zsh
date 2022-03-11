c19fab21b3b12a0036627ef771b199d6f8661374
Marko Korhonen <marko@korhonen.cc>
2022-03-11 18:14:21 +0200

    Format zsh files

--- home/.config/zsh/05-misc.zsh
+++ home/.config/zsh/05-misc.zsh
@@ -1,6 +1,6 @@
 # Launch tmux if logging in over ssh
 if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
-	tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
+    tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
 fi
 
 # share history between running zsh instances

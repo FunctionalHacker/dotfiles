278a7f247894f998b31d8315ce724c1432d6df4a
Marko Korhonen <marko@korhonen.cc>
2022-04-17 23:26:36 +0300

    Nvim treesitter: remove ensure_installed

--- home/.config/nvim/lua/pluginconf/treesitter.lua
+++ home/.config/nvim/lua/pluginconf/treesitter.lua
@@ -1,5 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = 'all',
     highlight = {enable = true},
     indent = {enable = true},
     incremental_selection = {enable = true},

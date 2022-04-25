4a72542f7fe2e775ac52c7e58746291790e39052
Marko Korhonen <marko@korhonen.cc>
2022-04-25 19:30:41 +0300

    Nvim: add treesitter ensure_installed

--- home/.config/nvim/lua/pluginconf/treesitter.lua
+++ home/.config/nvim/lua/pluginconf/treesitter.lua
@@ -1,13 +1,19 @@
 require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+        "bash", "c", "css", "dockerfile", "html", "http", "java", "json",
+        "json5", "latex", "lua", "make", "markdown", "php", "python", "regex",
+        "rst", "scss", "toml", "tsx", "typescript", "javascript", "yaml"
+    },
     highlight = {enable = true},
     indent = {enable = true},
     incremental_selection = {enable = true},
     context_commentstring = {enable = true}
 }
 
-vim.wo.foldmethod = 'expr';
+vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.wo.foldtext = [[substitute(getline(v:foldstart), '\\t' repeat('\ ',&tabstop),'g').'...'.trim(v:foldend)]]
+vim.wo.foldtext =
+    [[substitute(getline(v:foldstart), '\\t' repeat('\ ',&tabstop),'g').'...'.trim(v:foldend)]]
 vim.wo.fillchars = 'fold:\\'
 vim.wo.foldnestmax = 3
 vim.wo.foldminlines = 1

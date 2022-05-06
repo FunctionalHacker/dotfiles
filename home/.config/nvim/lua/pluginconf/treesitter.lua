df65e0b783a7e9780517aa688ad65cccefcb6577
Marko Korhonen <marko@korhonen.cc>
2022-05-06 13:32:32 +0300

    Nvim: simplify treesitter folding. Function name is displayed now

--- home/.config/nvim/lua/pluginconf/treesitter.lua
+++ home/.config/nvim/lua/pluginconf/treesitter.lua
@@ -12,8 +12,3 @@ require'nvim-treesitter.configs'.setup {
 
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.wo.foldtext =
-    [[substitute(getline(v:foldstart), '\\t' repeat('\ ',&tabstop),'g').'...'.trim(v:foldend)]]
-vim.wo.fillchars = 'fold:\\'
-vim.wo.foldnestmax = 3
-vim.wo.foldminlines = 1

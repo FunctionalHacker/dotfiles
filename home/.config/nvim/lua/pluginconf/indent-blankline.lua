3a918ea5fe45f1412b9a9c4943f2d4e47624fb04
Marko Korhonen <marko@korhonen.cc>
2022-08-14 20:36:32 +0300

    Neovim: add signature help plugin + make use of quotes consistent

--- home/.config/nvim/lua/pluginconf/indent-blankline.lua
+++ home/.config/nvim/lua/pluginconf/indent-blankline.lua
@@ -1,7 +1,7 @@
 vim.opt.list = true
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
+require('indent_blankline').setup {
+    space_char_blankline = ' ',
     show_current_context = true,
     show_current_context_start = true,
 }

ea6428143293143e1dae55bb17d21c6e45a6960a
Marko Korhonen <marko@korhonen.cc>
2022-08-17 21:45:14 +0300

    Add print for termux command not found handler

--- home/.config/zsh/03-aliases.zsh
+++ home/.config/zsh/03-aliases.zsh
@@ -42,6 +42,7 @@ function command_not_found_handler {
 }
 {%@@ if os == "termux" @@%}
 function command_not_found_handler {
+    printf 'zsh: command not found: %s\n' "$1"
 	$PREFIX/libexec/termux/command-not-found $1
 }
 {%@@ endif @@%}

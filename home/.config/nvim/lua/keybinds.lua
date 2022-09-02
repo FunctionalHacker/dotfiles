dab1d82be28e93e77988404964f8c49b244b5083
Marko Korhonen <marko@korhonen.cc>
2022-09-02 23:48:41 +0300

    Remove some redundant keymappings
    F.ex <C-j> to original keymapping <C-w>j to switch to left window

--- home/.config/nvim/lua/keybinds.lua
+++ home/.config/nvim/lua/keybinds.lua
@@ -1,21 +1,11 @@
 local map = vim.keymap.set
 
--- Telescope
---map('n', '<C-f>', '<cmd>Telescope find_files find_command=fd,-Ht,f<CR>')
---map('n', '<C-g>', '<cmd>Telescope live_grep<CR>')
-
 -- Navigate between buffers
 map('n', '<C-N>', vim.cmd.bn, {silent = true})
 map('n', '<C-B>', vim.cmd.bp, {silent = true})
 
--- Navigate between splits
-map('n', '<C-H>', '<C-W><C-H>')
-map('n', '<C-J>', '<C-W><C-J>')
-map('n', '<C-K>', '<C-W><C-K>')
-map('n', '<C-L>', '<C-W><C-L>')
-
 -- Run Neoformat
-map('n', '<M-f>', vim.cmd.Neoformat)
+map('n', '<M-f>', vim.cmd.Neoformat, {})
 
 -- Exit terminal insert mode with esc
-map('t', '<Esc>', '<C-\\><C-n>')
+map('t', '<Esc>', '<C-\\><C-n>', {})

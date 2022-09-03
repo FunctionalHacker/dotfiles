44a63ba8a47359418f6fd6d68144b086190f9d6d
Marko Korhonen <marko@korhonen.cc>
2022-09-03 14:11:09 +0300

    Zsh: move tmux lauch to it's own configuration file and make it the first one executed

---
+++ home/.config/zsh/01-tmux.zsh
@@ -0,0 +1,8 @@
+# Launch tmux if logging in over ssh
+if [[ -z "$TMUX" ]] && [[ -n "$SSH_CONNECTION" ]]; then
+	if tmux has-session -t ssh_tmux &2>/dev/null; then
+    	exec tmux -2 attach-session -t ssh_tmux
+	else
+		exec tmux -2 new-session -s ssh_tmux
+	fi
+fi
---
+++ home/.config/zsh/07-misc.zsh
@@ -0,0 +1,5 @@
+# share history between running zsh instances
+setopt share_history
+
+# ignore commands with leading space from history
+setopt histignorespace

1151b87fadf970a0399afe4a74a68f7478034a1c
Marko Korhonen <marko@korhonen.cc>
2022-09-03 16:19:47 +0300

    Fix variable scope in mason configuration

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -1,10 +1,10 @@
 local M = {}
 
-local opts = {mason = {}, lspconfig = {automatic_installation = true}}
+Opts = {mason = {}, lspconfig = {automatic_installation = true}}
 
-M.setup = function() require('mason').setup(opts.mason) end
+M.setup = function() require('mason').setup(Opts.mason) end
 
 M.lspconfig_setup =
-    function() require('mason-lspconfig').setup(opts.lspconfig) end
+    function() require('mason-lspconfig').setup(Opts.lspconfig) end
 
 return M

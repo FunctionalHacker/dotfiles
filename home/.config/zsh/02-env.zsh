939c0c14da863496de69859363857526688902cf
Marko Korhonen <marko@korhonen.cc>
2022-09-06 20:10:49 +0300

    Revert "Enable SSH agent forwarding for termux"
    This reverts commit 0deb2e37e0737549e923ace337bf76cd8dc7f848.

--- home/.config/zsh/02-env.zsh
+++ home/.config/zsh/02-env.zsh
@@ -61,11 +61,12 @@ export MANPAGER="$EDITOR +\"lua require 'pager'\" +Man!"
 export SYSTEMD_EDITOR=$EDITOR
 export SYSTEMD_PAGER=less
 
+{%@@ if os == "arch" or os == "ubuntu" @@%}
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
 	export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
-{%@@ if os == "termux" @@%}
-
+gpgconf --launch gpg-agent
+{%@@ elif os == "termux" @@%}
 eval $(okc-ssh-agent)
 {%@@ endif @@%}
 

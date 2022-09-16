86ba9538de0cb81e83891ea71665467631c0c35c
Marko Korhonen <marko@korhonen.cc>
2022-09-16 20:01:31 +0300

    Format all files with prettier, remove udiskie config

--- home/.config/imapnotify/marko@korhonen.cc.js
+++ home/.config/imapnotify/marko@korhonen.cc.js
@@ -1,19 +1,19 @@
-var child_process = require("child_process");
+var child_process = require('child_process');
 
 function getStdout(cmd) {
   var stdout = child_process.execSync(cmd);
   return stdout.toString().trim();
 }
 
-exports.host = "imap.migadu.com";
+exports.host = 'imap.migadu.com';
 exports.port = 993;
 exports.tls = true;
 exports.tlsOptions = {
   rejectUnauthorized: true,
 };
-exports.username = "marko@korhonen.cc";
+exports.username = 'marko@korhonen.cc';
 exports.password = "{{@@ env['PASS_EMAIL_MARKO'] @@}}";
-exports.onNewMail = "mbsync marko@korhonen.cc";
+exports.onNewMail = 'mbsync marko@korhonen.cc';
 exports.onNewMailPost =
-  "~/git/dotfiles/scripts/mail/notify-new-mail.sh marko@korhonen.cc";
-exports.boxes = ["INBOX"];
+  '~/git/dotfiles/scripts/mail/notify-new-mail.sh marko@korhonen.cc';
+exports.boxes = ['INBOX'];

4b8c40073ebde54ff69952dfd69acb05bfa6c362
Marko Korhonen <marko@korhonen.cc>
2023-05-02 09:24:46 +0300

    nvim: add dashboard to indent-blankline ignore filetype

--- home/.config/nvim/lua/plugins/indent-blankline.lua
+++ home/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,17 @@
 return function()
-	vim.opt.list = true
-	require("indent_blankline").setup({
-		space_char_blankline = " ",
-		show_current_context = true,
-		show_current_context_start = true,
-	})
+  vim.opt.list = true
+  require("indent_blankline").setup({
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+  })
+  vim.g.indent_blankline_filetype_exclude = {
+    "",
+    "checkhealth",
+    "dashboard",
+    "help",
+    "lspinfo",
+    "man",
+    "lazy",
+  }
 end

b28bef4c898cadaf6425541c8fcee3384da1a63b
Marko Korhonen <marko@korhonen.cc>
2023-05-08 11:22:05 +0300

    ZSH: Add clock alias

--- home/.config/zsh/04-aliases.zsh
+++ home/.config/zsh/04-aliases.zsh
@@ -291,3 +291,9 @@ dct() {
 }
 
 alias dslr-webcam='pkill -f gphoto2; gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'
+
+clock() {
+  while true; do
+    printf '%s\r' "$(date)"
+  done
+}

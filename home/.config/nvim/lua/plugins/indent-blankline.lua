b170d0182aae5c7b6f1587be4f94823646d7be05
Marko Korhonen <marko@korhonen.cc>
2023-09-29 09:03:03 +0300

    Nvim: migrate indent-blankline to v3

--- home/.config/nvim/lua/plugins/indent-blankline.lua
+++ home/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,18 +1,19 @@
 return function()
-  vim.opt.list = true
-  require("indent_blankline").setup({
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
+  require("ibl").setup({
+    exclude = {
+      filetypes = {
+        "",
+        "checkhealth",
+        "dashboard",
+        "git",
+        "gitcommit",
+        "TelescopePrompt",
+        "TelescopeResults",
+        "help",
+        "lazy",
+        "lspinfo",
+        "man",
+      },
+    },
   })
-  vim.g.indent_blankline_filetype_exclude = {
-    "",
-    "checkhealth",
-    "dashboard",
-    "help",
-    "lspinfo",
-    "man",
-    "lazy",
-    "git",
-  }
 end

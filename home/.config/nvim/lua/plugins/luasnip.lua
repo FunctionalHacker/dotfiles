aef2e0a750df93d40c5d02a19b6663cb9fe121b7
Marko Korhonen <marko@korhonen.cc>
2023-10-04 13:17:43 +0300

    Nvim: fix luasnip keymappings

--- home/.config/nvim/lua/plugins/luasnip.lua
+++ home/.config/nvim/lua/plugins/luasnip.lua
@@ -1,23 +1,4 @@
 return function()
-	local luasnip = require("luasnip")
-	local wk = require("which-key")
-
-	-- load friendly-snippets to luasnip
-	require("luasnip/loaders/from_vscode").lazy_load()
-
-	-- Register snippet navigation keybindings
-	local snippet_mappings = {
-		["<c-j>"] = {
-			function()
-				luasnip.jump(1)
-			end,
-		},
-		["<c-k>"] = {
-			function()
-				luasnip.jump(-1)
-			end,
-		},
-	}
-	--wk.register(snippet_mappings, {mode = "i"})
-	--wk.register(snippet_mappings, {mode = "s"})
+  -- load friendly-snippets to luasnip
+  require("luasnip/loaders/from_vscode").lazy_load()
 end

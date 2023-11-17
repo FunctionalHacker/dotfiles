92ab50ddfcbf8657f593bf9c0143e143c426b319
Marko Korhonen <marko@korhonen.cc>
2023-11-17 16:48:09 +0200

    Neovim: move navic to statusbar

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,7 @@
 return function()
   require("lualine").setup({
     sections = {
-      lualine_y = {},
-      lualine_z = {},
-    },
-    winbar = {
-      lualine_a = { "progress" },
-      lualine_c = {
-        "navic",
-        color_correction = nil,
-        navic_opts = nil,
-      },
-      lualine_z = { "location" },
+      lualine_c = { "navic" },
     },
   })
 end

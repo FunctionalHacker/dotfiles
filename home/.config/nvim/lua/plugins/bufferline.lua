61577153beaa98a2eb6f897b83cdeab72eb080bf
Marko Korhonen <marko@korhonen.cc>
2023-11-18 22:31:29 +0200

    Add diagnostics to bufferline

--- home/.config/nvim/lua/plugins/bufferline.lua
+++ home/.config/nvim/lua/plugins/bufferline.lua
@@ -2,4 +2,13 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = { "kyazdani42/nvim-web-devicons" },
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end,
+    },
+  },
 }

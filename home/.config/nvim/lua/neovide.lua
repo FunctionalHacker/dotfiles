f390929f7a5d6e22491349424b9dbebeaf1ce950
Marko Korhonen <marko@korhonen.cc>
2023-12-01 13:12:38 +0200

    Neovide: disable hide mouse

--- home/.config/nvim/lua/neovide.lua
+++ home/.config/nvim/lua/neovide.lua
@@ -13,8 +13,8 @@ vim.keymap.set("n", "<C-->", function()
   change_scale_factor(1 / 1.25)
 end)
 
--- Hide mouse when typing in neovide
-g.neovide_hide_mouse_when_typing = true
+-- Hide mouse when typing in neovide (disabled)
+g.neovide_hide_mouse_when_typing = false
 
 -- Enable cursor particles in neovide
 g.neovide_cursor_vfx_mode = "railgun"

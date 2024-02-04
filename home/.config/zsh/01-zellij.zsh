ea273128c60a97dad1b8af671d7b55390cb6e9b1
Marko Korhonen <marko@korhonen.cc>
2024-02-04 10:27:08 +0200

    Fix zellij SSH attach

--- home/.config/zsh/01-zellij.zsh
+++ home/.config/zsh/01-zellij.zsh
@@ -1,7 +1,7 @@
 if [[ -z "$ZELLIJ" ]]; then
   # Launch or attach to existing session if logging in over ssh
   if [[ -n "$SSH_CONNECTION" ]]; then
-    exec zellij attach -s SSH
+    exec zellij attach -c SSH
   # Launch a new local session otherwise
   else
     exec zellij

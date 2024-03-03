4f2f79407203f10366ee9bc4c419dedf527f7854
Marko Korhonen <marko@korhonen.cc>
2024-03-03 09:52:53 +0200

    Only launch zellij in SSH

--- home/.config/zsh/01-zellij.zsh
+++ home/.config/zsh/01-zellij.zsh
@@ -1,9 +1,4 @@
-if [[ -z "$ZELLIJ" ]]; then
-  # Launch or attach to existing session if logging in over ssh
-  if [[ -n "$SSH_CONNECTION" ]]; then
-    exec zellij attach -c SSH
-  # Launch a new local session otherwise
-  else
-    exec zellij
-  fi
+  # Launch or attach zellij to existing session if logging in over ssh
+if [[ -z "$ZELLIJ" && -n "$SSH_CONNECTION" ]]; then
+  exec zellij attach -c SSH
 fi

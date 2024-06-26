2f74592bff775ed21c4d6f9903da10db0de76ff1
Marko Korhonen <marko@korhonen.cc>
2024-06-26 11:34:00 +0300

    NeoVim: lualine: show selectioncount in visual mode

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -3,5 +3,25 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "kyazdani42/nvim-web-devicons" },
+  opts = {
+    sections = {
+      lualine_z = {
+        {
+          "selectioncount",
+          cond = function()
+            local mode = vim.fn.mode()
+            return mode == "v" or mode == "V" or mode == "\22"
+          end,
+        },
+        {
+          "location",
+          cond = function()
+            local mode = vim.fn.mode()
+            return mode ~= "v" and mode ~= "V" and mode ~= "\22"
+          end,
+        },
+      },
+    },
+  },
   config = true,
 }

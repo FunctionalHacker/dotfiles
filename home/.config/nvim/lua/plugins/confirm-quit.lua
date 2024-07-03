29fc562c129a0b6963056afe7ba3c9d441b3b957
Marko Korhonen <marko@korhonen.cc>
2024-07-03 09:23:43 +0300

    Nvim: use cond instead of enabled to disable confirm-quit

--- home/.config/nvim/lua/plugins/confirm-quit.lua
+++ home/.config/nvim/lua/plugins/confirm-quit.lua
@@ -1,7 +1,7 @@
 -- Confirm before quit
 --- @type LazyPluginSpec
 return {
-  enabled = vim.g.neovide == not nil,
+  cond = vim.g.neovide == not nil,
   "yutkat/confirm-quit.nvim",
   event = "CmdlineEnter",
   opts = {

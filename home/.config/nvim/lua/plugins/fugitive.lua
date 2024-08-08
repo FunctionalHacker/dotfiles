e9d20f005d9120eff3832a02d87d03684f4856bf
Marko Korhonen <marko@korhonen.cc>
2024-08-08 11:45:57 +0300

    Nvim: add gitea plugin for fugitive

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,12 @@
 -- Git commands
 --- @type LazyPluginSpec
-return { "tpope/vim-fugitive" }
+return {
+  "tpope/vim-fugitive",
+  dependencies = { "borissov/fugitive-gitea" },
+  config = function()
+    vim.g.fugitive_gitea_domains = {
+      "https://git.korhonen.cc",
+      "https://git.rossum.fi",
+    }
+  end,
+}

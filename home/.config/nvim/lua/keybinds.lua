1db002a9cb47aed3263ad7808c213a412f8b0a23
Marko Korhonen <marko@korhonen.cc>
2024-08-20 15:29:28 +0300

    Nvim: add keybind for alternate file

--- home/.config/nvim/lua/keybinds.lua
+++ home/.config/nvim/lua/keybinds.lua
@@ -9,6 +9,7 @@ end, { desc = "Toggle background between dark and light" })
 
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "Turn off search highlight" })
 vim.keymap.set("n", "<leader>co", '<cmd>silent! execute "%bd|e#|bd#"<cr>', { desc = "Close other buffers" })
+vim.keymap.set("n", "<leader>a", "<cmd>e#<cr>", { desc = "Edit alternate file" })
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal insert mode with esc" })

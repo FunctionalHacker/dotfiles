c91f32342a65f5e8224e842837ede7e38aac0adb
Marko Korhonen <marko@korhonen.cc>
2024-09-17 22:52:10 +0300

    Misc improvements

--- gpg.nix
+++ gpg.nix
@@ -15,10 +15,4 @@
       pkgs.yubikey-personalization
     ];
   };
-
-  environment.shellInit = ''
-    gpg-connect-agent /bye
-    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-    export MARKON_MUUTTUJA="test"
-  '';
 }

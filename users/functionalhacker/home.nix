0c00ca07ad1d416c28881b36283188b01d2ddd2e
Marko Korhonen <marko@korhonen.cc>
2024-09-18 23:02:29 +0300

    Add gpg configuration to git

--- users/functionalhacker/home.nix
+++ users/functionalhacker/home.nix
@@ -12,7 +12,6 @@
       fd
       gcc
       neovide
-      neovim
       nerdfonts
       nodejs
       pass
@@ -110,6 +109,23 @@
       enable = true;
       userName = "Marko Korhonen";
       userEmail = "marko@korhonen.cc";
+
+      extraConfig = {
+        user.signingkey = "F2B9F713E7ED627336DD85E444FA3F28CD931BB0";
+        commit = {
+          gpgsign = true;
+          signoff = true;
+        };
+        tag = {
+          gpgsign = true;
+          signoff = true;
+        };
+        merge = {
+          prompt = false;
+          conflictstyle = "diff3";
+          tool = "nvim";
+        };
+      };
     };
   };
 }

c94dbb7c33aa57be6646b15059172e318153e1b1
Marko Korhonen <marko@korhonen.cc>
2024-09-18 23:01:43 +0300

    Add plymouth

--- nixos_common.nix
+++ nixos_common.nix
@@ -35,7 +35,30 @@
         enable = true;
         efiSupport = true;
         device = "nodev";
+        timeoutStyle = "hidden";
       };
+    };
+
+    initrd = {
+      systemd.enable = true;
+      verbose = false;
+    };
+
+    consoleLogLevel = 0;
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    plymouth = {
+      enable = true;
+      theme = "bgrt";
     };
   };
 

45047a732067edea1f549d66bfe1ee0cddf284ec
Marko Korhonen <marko@korhonen.cc>
2024-09-18 23:18:07 +0300

    Switch back to systemd-boot
    It allows for a smoother transition to plymouth

--- nixos_common.nix
+++ nixos_common.nix
@@ -31,12 +31,8 @@
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        efiSupport = true;
-        device = "nodev";
-        timeoutStyle = "hidden";
-      };
+      systemd-boot.enable = true;
+      timeout = 0;
     };
 
     initrd = {

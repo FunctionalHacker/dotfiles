67e5ee2d12444154ed452c6906230b23a29c27dd
Marko Korhonen <marko@korhonen.cc>
2024-09-28 20:53:34 +0300

    Add early kms for Mirkwood

--- hosts/Mirkwood/configuration.nix
+++ hosts/Mirkwood/configuration.nix
@@ -8,5 +8,10 @@
 
   networking.hostName = "Mirkwood";
 
-  boot.initrd.luks.devices."luks-cf2ca44e-d8d1-4ec8-a072-921f29ed693d".device = "/dev/disk/by-uuid/cf2ca44e-d8d1-4ec8-a072-921f29ed693d";
+  boot = {
+    initrd = {
+      luks.devices."luks-cf2ca44e-d8d1-4ec8-a072-921f29ed693d".device = "/dev/disk/by-uuid/cf2ca44e-d8d1-4ec8-a072-921f29ed693d";
+      kernelModules = [ "amdgpu" ];
+    };
+  };
 }

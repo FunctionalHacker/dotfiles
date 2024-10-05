06e6ff5e66204c161664212f2b700dd2064eb7e7
Marko Korhonen <marko@korhonen.cc>
2024-10-05 16:16:27 +0300

    Change home-manager backup extension

--- home-manager.nix
+++ home-manager.nix
@@ -5,7 +5,7 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs; };
-    backupFileExtension = "bak";
+    backupFileExtension = "hm-backup";
     users.functionalhacker =
       import ./users/functionalhacker/home.nix;
   };

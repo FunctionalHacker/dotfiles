9d9c7453758982228c3448737089371ad09e82af
Marko Korhonen <marko@korhonen.cc>
2024-10-05 16:19:44 +0300

    Add authorized SSH key

--- users/functionalhacker/configuration.nix
+++ users/functionalhacker/configuration.nix
@@ -6,5 +6,8 @@
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLUrf0LpumX6XEkPW6Z105sgJMWWOllWy7sxB+lgdUw Marko YubiKey"
+    ];
   };
 }

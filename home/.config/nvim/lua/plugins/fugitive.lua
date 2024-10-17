9c92e2305b10ee7b3c277f7bf2677e4c789b6533
Marko Korhonen <marko@korhonen.cc>
2024-10-17 10:04:51 +0300

    Neovim: add more git keybinds

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,5 @@
+local k = vim.keymap.set
+
 -- Git commands
 --- @type LazyPluginSpec
 return {
@@ -8,5 +10,30 @@ return {
       "https://git.korhonen.cc",
       "https://git.rossum.fi",
     }
+
+    -- Keybinds
+    k("n", "<leader>gs", function()
+      vim.cmd("Git status")
+    end, { desc = "Status" })
+
+    k("n", "<leader>gaa", function()
+      vim.cmd("Git add -A")
+    end, { desc = "All" })
+
+    k("n", "<leader>gc", function()
+      vim.cmd("Git commit")
+    end, { desc = "Commit" })
+
+    k("n", "<leader>gu", function()
+      vim.cmd("Git push")
+    end, { desc = "Push" })
+
+    k("n", "<leader>gd", function()
+      vim.cmd("Git pull")
+    end, { desc = "Pull" })
+
+    k({ "n", "x" }, "<leader>gb", function()
+      vim.cmd("GBrowse")
+    end, { desc = "Browse" })
   end,
 }

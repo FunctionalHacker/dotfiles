9c73eb4fea99f3c65c282bd66be25822f0ff7566
Marko Korhonen <marko@korhonen.cc>
2025-07-18 10:22:08 -0500

    Nvim: add lazygit

---
+++ home/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+}

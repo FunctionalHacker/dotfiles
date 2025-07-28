996a4526d209cc75710706ddb894511213b3161e
Marko Korhonen <marko@korhonen.cc>
2025-07-28 09:08:09 -0500

    Nvim: Match fugitive keybinds with lazygit

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -23,11 +23,11 @@ return {
       vim.cmd("Git commit")
     end, { desc = "Commit" })
 
-    k("n", "<leader>gu", function()
+    k("n", "<leader>gP", function()
       vim.cmd("Git push")
     end, { desc = "Push" })
 
-    k("n", "<leader>gd", function()
+    k("n", "<leader>gp", function()
       vim.cmd("Git pull")
     end, { desc = "Pull" })
 

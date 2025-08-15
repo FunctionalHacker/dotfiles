7040f33349a5caa1aaaf030b20016acf47c6a4a4
Marko Korhonen <marko@korhonen.cc>
2025-08-15 11:34:10 -0500

    Nvim: migrate deprecated vim.diagnostic calls

--- home/.config/nvim/lua/lsp_utils.lua
+++ home/.config/nvim/lua/lsp_utils.lua
@@ -4,6 +4,7 @@ local m = {}
 
 local lsp = vim.lsp
 local diagnostic = vim.diagnostic
+local jump = diagnostic.jump
 local k = vim.keymap.set
 
 -- Maps LSP specific keybinds.
@@ -26,8 +27,12 @@ function m.map_keys()
   end, { desc = "List folders" })
   k("n", "<leader>wr", lsp.buf.remove_workspace_folder, { desc = "Remove folder" })
   k("n", "K", lsp.buf.hover, { desc = "Hover" })
-  k("n", "[d", diagnostic.goto_prev, { desc = "Previous diagnostic" })
-  k("n", "]d", diagnostic.goto_next, { desc = "Next diagnostic" })
+  k("n", "[d", function()
+    jump({ count = -1, float = true })
+  end, { desc = "Previous diagnostic" })
+  k("n", "]d", function()
+    jump({ count = 1, float = true })
+  end, { desc = "Next diagnostic" })
   k("n", "gD", lsp.buf.declaration, { desc = "Declaration" })
   k("n", "gd", builtin.lsp_definitions, { desc = "Definition" })
   k("n", "gi", builtin.lsp_implementations, { desc = "Implementation" })

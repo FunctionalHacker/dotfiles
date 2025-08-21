c9797061505e13b0f679b4bb5873091e92d2a5aa
Marko Korhonen <marko@korhonen.cc>
2025-08-21 00:21:35 -0500

    Nvim: add some custom sections to dashboard

--- home/.config/nvim/lua/plugins/snacks/dashboard.lua
+++ home/.config/nvim/lua/plugins/snacks/dashboard.lua
@@ -3,6 +3,31 @@ return {
   "snacks.nvim",
   ---@type snacks.Config
   opts = {
-    dashboard = {},
+    dashboard = {
+      preset = {
+        keys = {
+          {
+            icon = "📽",
+            key = "<leader>p",
+            desc = "Open a project",
+            action = ":lua Snacks.dashboard.pick('projects')",
+          },
+          { icon = " ", key = "<C-f>", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "<C-g>", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "e", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.expand('~/git/dotfiles/home/.config/nvim/lua')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = "📦 ", key = "M", desc = "Mason", action = ":Mason" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
   },
 }

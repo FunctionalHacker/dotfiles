47e5f47a7a2b1d2463cf931ea35d6fd9f12f3929
Marko Korhonen <marko@korhonen.cc>
2025-08-22 14:07:23 -0500

    Nvim: Explorer: add custom action to duplicate file, change keybind

--- home/.config/nvim/lua/plugins/snacks/explorer.lua
+++ home/.config/nvim/lua/plugins/snacks/explorer.lua
@@ -3,16 +3,43 @@ return {
   "snacks.nvim",
   ---@type snacks.Config
   opts = {
-    explorer = {},
+    explorer = {
+      replace_netrw = true,
+    },
     picker = {
       sources = {
-        explorer = {},
+        explorer = {
+          win = {
+            list = {
+              keys = {
+                ["D"] = "duplicate_file",
+              },
+            },
+          },
+          actions = {
+            duplicate_file = {
+              action = function(_, item)
+                if not item then
+                  return
+                end
+
+                local new_name = vim.fn.input("Duplicate as: ", item.file)
+                if new_name == "" or vim.fn.filereadable(new_name) == 1 then
+                  vim.notify("Invalid name or file exists", vim.log.levels.WARN)
+                  return
+                end
+                vim.uv.fs_copyfile(item.file, new_name)
+                vim.notify("Duplicated to " .. new_name)
+              end,
+            },
+          },
+        },
       },
     },
   },
   keys = {
     {
-      "<leader>o",
+      "<leader>e",
       function()
         Snacks.explorer()
       end,

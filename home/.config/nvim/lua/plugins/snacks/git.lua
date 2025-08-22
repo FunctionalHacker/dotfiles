411368ae302f3f789fb4d7d2051e96e00dac18c5
Marko Korhonen <marko@korhonen.cc>
2025-08-22 14:10:53 -0500

    Nvim: add git blame line from snacks

--- home/.config/nvim/lua/plugins/snacks/git.lua
+++ home/.config/nvim/lua/plugins/snacks/git.lua
@@ -3,6 +3,15 @@ return {
   "snacks.nvim",
   ---@type snacks.Config
   opts = {
-    indent = {},
+    git = {},
+  },
+  keys = {
+    {
+      "<leader>gb",
+      function()
+        Snacks.git.blame_line()
+      end,
+      desc = "Blame",
+    },
   },
 }

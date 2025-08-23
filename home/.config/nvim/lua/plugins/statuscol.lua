7fb2d2294b250f81ec287400876299c274165155
Marko Korhonen <marko@korhonen.cc>
2025-08-22 19:35:42 -0500

    Nvim: improve folds appearance

--- home/.config/nvim/lua/plugins/statuscol.lua
+++ home/.config/nvim/lua/plugins/statuscol.lua
@@ -2,5 +2,18 @@
 --- @type LazyPluginSpec
 return {
   "luukvbaal/statuscol.nvim",
-  config = true,
+  config = function()
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      segments = {
+        { text = { "%s" }, click = "v:lua.ScSa" },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        {
+          text = { " ", builtin.foldfunc, " " },
+          condition = { builtin.not_empty, true, builtin.not_empty },
+          click = "v:lua.ScFa",
+        },
+      },
+    })
+  end,
 }

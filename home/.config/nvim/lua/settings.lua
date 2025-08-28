fda579a3bee5b4a604a73b80c03d93ccac27a71e
Marko Korhonen <marko@korhonen.cc>
2025-08-28 09:15:38 -0500

    Nvim: disable legacy providers

--- home/.config/nvim/lua/settings.lua
+++ home/.config/nvim/lua/settings.lua
@@ -51,3 +51,9 @@ o.shiftwidth = 4
 o.smartindent = true
 
 o.showmode = false
+
+-- Disable unused legacy providers
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0

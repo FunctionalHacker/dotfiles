2dac5076f2431467e325e762e201299f7d66402a
Marko Korhonen <marko@korhonen.cc>
2025-08-29 09:42:09 -0500

    Nvim: mini moved to a github organization

--- home/.config/nvim/lua/plugins/mini/init.lua
+++ home/.config/nvim/lua/plugins/mini/init.lua
@@ -1,7 +1,7 @@
 -- Library of 30+ independent Lua modules improving overall Neovim experience with minimal effort
 --- @type LazyPluginSpec
 return {
-  "echasnovski/mini.nvim",
+  "nvim-mini/mini.nvim",
   version = "*",
   event = "VeryLazy",
   import = "plugins.mini",

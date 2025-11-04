292f7b718ee6729202f120449784c3d6750a974c
Marko Korhonen <marko@korhonen.cc>
2025-11-04 15:47:04 -0600

    Nvim: set up code companion

--- home/.config/nvim/lua/plugins/codecompanion.lua
+++ home/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,8 +1,37 @@
 -- ✨ AI Coding, Vim Style
 return {
   "olimorris/codecompanion.nvim",
-  opts = {},
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "copilot",
+        slash_commands = {
+          ["file"] = {
+            opts = {
+              provider = "snacks",
+            },
+          },
+          ["buffer"] = {
+            opts = {
+              provider = "snacks",
+            },
+          },
+        },
+      },
+      inline = { adapter = "copilot" },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
+    -- Only for authenticating
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      build = ":Copilot auth",
+      opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      },
+    },
   },
 }

8ea50f28a5ed9ac312cac5edb3979d23e5eb3260
Marko Korhonen <marko@korhonen.cc>
2025-11-10 08:57:56 -0600

    Nvim: restructure mason config

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -1,25 +1,39 @@
 -- Portable package manager for Neovim that runs everywhere Neovim runs.
 -- Easily install and manage LSP servers, DAP servers, linters, and formatters
---- @type LazyPluginSpec
+--- @type LazyPluginSpec[]
 return {
-  "mason-org/mason-lspconfig.nvim",
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    "neovim/nvim-lspconfig",
-    -- Add support for LSP file operations
-    { "antosha417/nvim-lsp-file-operations", opts = {} },
-    -- Add MasonUpdateAll
-    { "Zeioth/mason-extra-cmds", opts = {} },
-    -- Add lockfile support
-    {
-      "zapling/mason-lock.nvim",
-      opts = {
-        lockfile_path = vim.fn.expand("~/git/dotfiles/home/.config/nvim/mason-lock.json"),
-      },
+  { "mason-org/mason.nvim", opts = {} },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
     },
-    -- Automatically fetch schemas from JSON schema store
+  },
+  -- Automatically fetch schemas from JSON schema store
+  {
     "b0o/schemastore.nvim",
+    event = "VeryLazy",
+  },
+  -- Add support for LSP file operations
+  {
+    "antosha417/nvim-lsp-file-operations",
+    event = "VeryLazy",
+    opts = {},
+  },
+  -- Add MasonUpdateAll
+  {
+    "Zeioth/mason-extra-cmds",
+    event = "VeryLazy",
+    opts = {},
+  },
+  -- Add lockfile support
+  {
+    "zapling/mason-lock.nvim",
+    opts = {
+      lockfile_path = vim.fn.expand("~/git/dotfiles/home/.config/nvim/mason-lock.json"),
+    },
   },
 }

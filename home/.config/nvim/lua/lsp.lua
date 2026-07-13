a8e5252613cd06e286a7042320af0750ea696286
Marko Korhonen <marko@korhonen.cc>
2026-07-13 12:39:22 +0300

    Nvim LSP: enable inlay hints and document color

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -1,3 +1,7 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
 -- Add LSP capabilities
 vim.lsp.config("*", {
   capabilities = require("blink.cmp").get_lsp_capabilities({
@@ -9,4 +13,54 @@ vim.lsp.config("*", {
       },
     },
   }, true),
+})
+
+vim.lsp.config("ts_ls", {
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup("lsp_attach"),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local buf = args.buf
+    if client then
+      -- Inlay hints
+      -- if client:supports_method("textDocument/inlayHint") then
+      --   vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+      -- end
+
+      -- Colorizer
+      if client:supports_method("textDocument/documentColor") then
+        vim.lsp.document_color.enable(true, { bufnr = buf }, {
+          style = "virtual",
+        })
+      end
+    end
+  end,
 })

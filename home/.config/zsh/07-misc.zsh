12f6ba563764eab71d4b274c91d3792f74b00ce5
Marko Korhonen <marko@korhonen.cc>
2026-09-23 19:33:21 -0500

    ZSH: Fix error in Termux

--- home/.config/zsh/07-misc.zsh
+++ home/.config/zsh/07-misc.zsh
@@ -6,3 +6,6 @@ setopt histignorespace
 
 # Enable going backwards in history
 setopt no_flow_control
+
+# Fix error on Termux https://github.com/jeffreytse/zsh-vi-mode/issues/159#issuecomment-1871335866
+setopt re_match_pcre
